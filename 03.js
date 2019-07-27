69bb833dc134c817a130091432128aa37af1f68b
prasetyo-adi <52805350+prasetyo-adi@users.noreply.github.com>
2019-07-27 20:00:17 +0700

    Add files via upload

---
+++ 03.js
@@ -0,0 +1,27 @@
+
+var s = ''; 
+
+for (var i=0; i<2; i++) {
+		s += '*';
+		for (var j =0; j<1; j++){
+			s+=' = = = * ';
+		}
+		s+= '\n';
+	}
+
+for(var x = 0; x<5; x++) {
+	s+='* ' ;
+}
+s+= '\n';
+for (var i=0; i<2; i++) {
+		s += '*';
+		for (var j =0; j<1; j++){
+			s+=' = = = * ';
+		}
+		s+= '\n';
+	}
+
+	console.log(s);
+
+
+		
